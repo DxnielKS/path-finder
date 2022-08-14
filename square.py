b390f69fc20e1ea37ef73e761e71f19f08eca6b1
DKS <daniel.saisani@kcl.ac.uk>
2022-08-14 17:51:33 +0100

    Update square.py

--- square.py
+++ square.py
@@ -1,5 +1,19 @@
 class Square():
-    def __init__(self,size):
+    def __init__(self,size,x,y):
         self.size=size
+        self.x=x
+        self.y=y
+        self.traversed = False
+        self.wall = False
     def get_size(self):
         return self.size
+    def get_x(self):
+        return self.x
+    def get_y(self):
+        return self.y
+    def is_taversed(self):
+        return self.traversed
+    def is_wall():
+        return self.wall
+    def toggle_traversed(self):
+        self.traversed=True
