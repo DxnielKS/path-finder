72cd0007193021d59aa252d0ae4848a5addd6fb3
DKS <daniel.saisani@kcl.ac.uk>
2022-08-14 15:35:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,24 @@ pygame.init()
 width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
 screen = pygame.display.set_mode((width//2,height//2))
 
+WHITE = (255, 255, 255)
+BLACK = (0,0,0)
+squares = []
+
+def drawGrid():
+    for i in range(0,width, 30):
+        # print(i)
+        for j in range(0, height, 30):
+            # print(j)
+            pygame.draw.rect(screen, BLACK, pygame.Rect(i, j, 30, 30),2)
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((255, 255, 255))
+    screen.fill(WHITE)
+    drawGrid()
+    pygame.display.flip() # Flip the display
 
-
-    # Flip the display
-    pygame.display.flip()
 pygame.quit()
