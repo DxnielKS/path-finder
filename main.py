356819dc87e6dda64aa3914e9432d41efe5d9a73
DKS <daniel.saisani@kcl.ac.uk>
2022-08-14 17:51:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 import pygame
+from square import Square
 pygame.init()
 
-width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
-screen = pygame.display.set_mode((width//2,height//2))
+##############################################################################################################
 
 WHITE = (255, 255, 255)
 BLACK = (0,0,0)
-squares = []
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
 
-def drawGrid():
+squares = []
+square_thickness = 2
+square_size = 30
+
+##############################################################################################################
+
+def draw_grid(): 
     for i in range(0,width, 30):
         # print(i)
         for j in range(0, height, 30):
             # print(j)
-            pygame.draw.rect(screen, BLACK, pygame.Rect(i, j, 30, 30),2)
+            pygame.draw.rect(screen, BLACK, pygame.Rect(i, j, square_size, square_size),square_thickness)
+            squares.append(Square(square_size,i,j))
+    # print(squares)
+
+##############################################################################################################
+
+width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
+screen = pygame.display.set_mode((width//2,height//2))
+screen.fill(WHITE)
+draw_grid()
+
+##############################################################################################################
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill(WHITE)
-    drawGrid()
     pygame.display.flip() # Flip the display
 
 pygame.quit()
